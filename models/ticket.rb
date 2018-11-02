decf15534c773179ef3f957d5cf5a25adc64648d
Thomas McPhee <mcp.thom@gmail.com>
2018-11-02 17:32:34 +0000

    missed a save

--- models/ticket.rb
+++ models/ticket.rb
@@ -12,28 +12,28 @@ attr_reader :id, :customer_id, :film_id
   end
 
   def save()
-    sql = "INSERT INTO customers (name, funds) VALUES ($1, $2) RETURNING id;"
-    values = [@name, @funds]
-    customer = SqlRunner.run(sql, values).first
-    @id = customer['id'].to_i
+    sql = "INSERT INTO tickets (customer_id, film_id) VALUES ($1, $2) RETURNING id;"
+    values = [@customer_id, @film_id]
+    ticket = SqlRunner.run(sql, values).first
+    @id = ticket['id'].to_i
   end
 
   def self.all
-    sql = "SELECT * FROM customers;"
+    sql = "SELECT * FROM tickets;"
     values = []
-    customers = SqlRunner.run(sql, values)
-    result = customers.map { | customer | Customer.new(customer) }
+    tickets = SqlRunner.run(sql, values)
+    result = tickets.map { | ticket | Ticket.new(ticket) }
     return result
   end
 
   def update()
-    sql = "UPDATE customers SET (name, funds) = ( $1, $2 ) WHERE id = $3;"
-    values = [@name, @funds, @id]
+    sql = "UPDATE tickets SET (customer_id, film_id) = ( $1, $2 ) WHERE id = $3;"
+    values = [@customer_id, @film_id, @id]
     SqlRunner.run(sql, values)
   end
 
   def self.delete_all()
-    sql = "DELETE FROM customers;"
+    sql = "DELETE FROM tickets;"
     values = []
     SqlRunner.run(sql, values)
   end
